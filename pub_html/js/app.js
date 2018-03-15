a3ff724ce277168fd97a1d4a8c44a6bcb1962559
Unknown <tcjoshuafan@gmail.com>
2018-03-15 10:28:30 -0700

    progress

--- pub_html/js/app.js
+++ pub_html/js/app.js
@@ -1,1 +1,15 @@
-$(document).foundation()
+
+
+setInterval(function(){
+  $("#glitch-business").toggle();
+  setTimeout(function(){
+    $("#glitch-business").toggle();
+  }, 1000)
+},2000);
+
+setInterval(function(){
+  $("#glitch-ux").toggle();
+  setTimeout(function(){
+    $("#glitch-ux").toggle();
+  }, 1000)
+},2000);
